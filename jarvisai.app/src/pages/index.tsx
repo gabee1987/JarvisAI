db5f4e2513dbc9d0235600e287e0ab850b778801
gabee1987 <gabee1987@gmail.com>
2023-08-31 16:24:34 +0200

    modified the new default app structure in the frontend project to the old pages structure

---
+++ jarvisai.app/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">JARVIS AI</h1>
+    </div>
+  );
+}
