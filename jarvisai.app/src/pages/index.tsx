f57220f1b4dbc0f6e93dcbd99101487cc56ad234
gabee1987 <gabee1987@gmail.com>
2023-10-04 12:15:17 +0200

    created the basics for both the app and server, the voice input is working, the server can get messages

--- jarvisai.app/src/pages/index.tsx
+++ jarvisai.app/src/pages/index.tsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Page() {
+  const [transcript, setTranscript] = useState("");
+  const [response, setResponse] = useState("");
+
+  const askJarvis = async (text: string) => {
+    try {
+      const res = await fetch("/api/ask-jarvis", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text }),
+      });
+      const data = await res.json();
+      setResponse(data.reply);
+    } catch (error) {
+      console.error("Error communicating with backend:", error);
+    }
+  };
+
+  const handleListeningEnd = (text: string) => {
+    setTranscript(text);
+    askJarvis(text);
+  };
+
+  const startListening = () => {
+    const recognition = new (window as any).webkitSpeechRecognition();
+    recognition.lang = "en-US";
+    recognition.onresult = (event: any) => {
+      const text = event.results[0][0].transcript;
+      handleListeningEnd(text);
+    };
+    recognition.start();
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">JARVIS AI</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold mb-4">JARVIS AI</h1>
+      <button
+        onClick={startListening}
+        className="bg-blue-500 px-4 py-2 rounded"
+      >
+        Start Listening
+      </button>
+      {transcript && <p className="mt-4">{transcript}</p>}
+      {response && <p className="mt-4 text-green-400">{response}</p>}
     </div>
   );
 }
--- jarvisai.server/server.js
+++ jarvisai.server/server.js
@@ -1,9 +1,53 @@
-const express = require("express");
+import OpenAI from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+// import cors from 'cors';
+
+// require("dotenv").config();
+
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from JarvisAI Backend!");
+// Middleware
+// app.use(cors());
+app.use(bodyParser.json());
+
+// OpenAI Configuration
+const openai = new OpenAI({ apiKey: process.env.JARVISAI_API_KEY });
+
+// Chat history to maintain context
+const chatHistory = [];
+
+app.post("/ask-jarvis", async (req, res) => {
+  const user_input = req.body.message;
+
+  // Prepare the message list with chat history
+  const messageList = chatHistory.map(([input_text, completion_text]) => ({
+    role: input_text === "user" ? "ChatGPT" : "user",
+    content: input_text,
+  }));
+  messageList.push({ role: "user", content: user_input });
+
+  try {
+    const GPTOutput = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: messageList,
+    });
+
+    const output_text = GPTOutput.data.choices[0].message.content;
+    chatHistory.push([user_input, output_text]);
+
+    res.json({ message: output_text });
+  } catch (err) {
+    if (err.response) {
+      console.error(err.response.status);
+      console.error(err.response.data);
+      res.status(err.response.status).json(err.response.data);
+    } else {
+      console.error(err.message);
+      res.status(500).json({ error: err.message });
+    }
+  }
 });
 
 app.listen(PORT, () => {
